0917299051518157e8be609152de87636af8b0b6
Juhász Viktória 13c <116707126+JuhaszViktoriaa@users.noreply.github.com>
2026-01-22 12:18:42 +0100

    Add files via upload

---
+++ szamnegyes/src/components/Counter.vue
@@ -0,0 +1,15 @@
+<template>
+    <form @submit.prevent="store.save()">
+        <input type="text" v-model="store.tarolo" placeholder="szamnegyes elso szama">
+        <input type="text" v-model="store.tarolo" placeholder="szamnegyes masodik szama">
+        <input type="text" v-model="store.tarolo" placeholder="szamnegyes harmadik szama">
+        <input type="text" v-model="store.tarolo" placeholder="szamnegyes negyedik szama">
+
+        <p class="btn btn-success text-light">rogzit</p>
+    </form>
+</template>
+
+<script setup>
+    import { CounterStore } from '@/stores/counter';
+    const store = useCounterStore();
+</script>
